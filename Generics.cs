20d14fc5e6b2443f7da8ba28e767704daf1cd84e
sonu1239 <ss316742@gmail.com>
2021-10-24 00:59:08 +0530

    Adding Mux Int Num to Generics

---
+++ Generics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Generics
+    {
+        public static int MaxNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            return default;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generic problem !");
+            int Result = Generics.MaxNumber(15, 20, 25);
+            Console.WriteLine("{0} is max number", Result);
+            Console.ReadLine();
         }
     }
 }
